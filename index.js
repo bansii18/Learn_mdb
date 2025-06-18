cb1daadf74559379ab3cc6900a790591968a1b1c
bansii18 <bansi84451@gmail.com>
2025-06-18 10:16:51 +0530

    Schema

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ main()
 async function main(){
    await mongoose.connect('mongodb://127.0.0.1:27017/myDB');
 }
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    age : Number
+});
